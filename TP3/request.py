230f4fb56e297e3d8f6abf51151e2ce6971258f9
Thomas HAW <hawkwant@etu.univ-grenoble-alpes.fr>
2023-03-27 16:01:59 +0200

    TP3 - OK - fixup

--- TP3/request.py
+++ TP3/request.py
@@ -2,5 +2,8 @@ import json
 
 import requests
 
-request = requests.post("http://127.0.0.1:5000/sayhello", data=json.dumps({"message": "This doesn't work"}), headers={"Content-Type": "text/plain"})
+request = requests.post(
+    "http://127.0.0.1:5000/sayhello",
+    data=json.dumps({"message": "This doesn't work"}),
+    headers={"Content-Type": "text/plain"})
 print(request.headers)
