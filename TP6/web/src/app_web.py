ad899d75dad741fa488c5818eaa0195de5fce05c
Thomas HAW <hawkwant@etu.univ-grenoble-alpes.fr>
2023-04-24 16:08:49 +0200

    TP6 - 4

--- TP6/web/src/app_web.py
+++ TP6/web/src/app_web.py
@@ -1,12 +1,16 @@
 import json
+import os
 
 import flask
+import flask_wtf.csrf as csrf
 import pynats
 
 app = flask.Flask(__name__)
+app.config['SECRET_KEY'] = '08ead3fa939edb04acca31e043d63a0636deb946e5addc396a65f8ef4126d0b4'
+csrf.CSRFProtect(app)
 
 ACCOUNTS = ['59229', '94641', '44702', '93375']
-NATS_HOST = "nats://10.1.33.57:4222"
+NATS_HOST = os.getenv("NATS_HOST", "10.1.33.57")
 
 
 @app.route('/')
@@ -17,8 +21,9 @@ def home():
 @app.route("/envoi", methods=["POST"])
 def envoi():
     data = dict(flask.request.form)
+    flask.flash("Dépôt demandé")
 
-    with pynats.NATSClient(NATS_HOST) as client:
+    with pynats.NATSClient(f"nats://{NATS_HOST}:4222") as client:
         client.connect()
         client.publish(
             subject=f"bank.deposit.{data.get('account')}",
@@ -27,4 +32,4 @@ def envoi():
     return flask.redirect(flask.url_for("home"))
 
 if __name__ == '__main__':
-    app.run(debug=True, port=5000)
+    app.run(host="0.0.0.0", port=5000)
