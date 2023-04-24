c54f2699740ee211ee8a9907beb4b083bfc58ff9
Thomas HAW <hawkwant@etu.univ-grenoble-alpes.fr>
2023-04-24 14:02:20 +0200

    TP 6 - 3

---
+++ TP6/bank.sql
@@ -0,0 +1,6 @@
+CREATE TABLE account (number varchar(5) not null primary key, amount integer not null);
+
+INSERT INTO account VALUES ('59229', 500);
+INSERT INTO account VALUES ('94641', 200);
+INSERT INTO account VALUES ('44702', 5250);
+INSERT INTO account VALUES ('93375', 15874);
